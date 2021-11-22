8b66fc637fae7130ecf7c17c31e73c04fa296b0b
Ritesh Kumar <riteshchoudhary2605@gmail.com>
2021-11-22 17:35:02 +0530

    Added specs for models

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+ describe '#run' do
+   
+    context 'when #run' do
+      it 'is create movie' do
+        Movie.run
+
+        expect(Movie.count).not_to eq nil
+      end
+    end
+  end
+end
